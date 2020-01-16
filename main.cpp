8906bd088c9786ae63c70ab157db9e5dbc3a6400
VikSn0w <36678730+VikSn0w@users.noreply.github.com>
2020-01-16 13:37:31 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <windows.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <sstream>
+
+using namespace std;
+
+void FileCreations(string Name, string FileSize, int Copies);
+
+main()
+{
+    system("title Memory Dumper 1.0 - by viksn0w");
+    string ByteSize, NomeFile;
+    int choice;
+
+    printf("Name of the file: ");
+    cin  >> NomeFile;
+
+    printf("\nSize of the file (byte - 1MB =~ 1050000 Bytes): ");
+    cin  >> ByteSize;
+
+    printf("\nNumber of copies: ");
+    cin  >> choice;
+
+    FileCreations(NomeFile, ByteSize, choice);
+
+    system("pause");
+}
+
+void FileCreations(string Name, string FileSize, int Copies)
+{
+    string Final;
+    for(int i = 0; i < Copies; i++)
+    {
+        if(i == 0) Final = "fsutil file createnew " + Name + " " + FileSize;
+        else       Final = "fsutil file createnew " + Name + to_string(i) + " " + FileSize;
+        system(Final.c_str());
+    }
+}
